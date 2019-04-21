62a8bbd7733f553bfb8546cb33266124788c10da
EC2 Default User <ec2-user@ip-172-31-36-0.us-east-2.compute.internal>
2019-04-21 06:21:17 +0000

    added admin page

--- app/models/game_board.rb
+++ app/models/game_board.rb
@@ -1,27 +1,29 @@
 class GameBoard < ActiveRecord::Base
     has_and_belongs_to_many :categories
     belongs_to :game
-    has_and_belongs_to_many :categories
-    belongs_to :game
     has_many :clues, :through => :categories
-
+    
     @@num_categories = 6
-
+    
     #Loads categories into gameboard
     def getCategories
-       if self.game_type == "double"
-           value_filter = 1000
-       else
-           value_filter = 100
-       end
-
-       self.categories << Category.find_by_sql(
-           "SELECT * FROM categories
-            WHERE api_category_id IN
-               (SELECT api_category_id FROM clues
-               WHERE value = " + value_filter.to_s + ")
-            ORDER BY Random()
-            LIMIT " + @@num_categories.to_s)
-       self.save()
+        if self.game_type == "double"
+            value_filter = 1000
+        else
+            value_filter = 100
+        end
+        
+        self.categories << Category.find_by_sql(
+            "SELECT * FROM categories
+             WHERE id IN
+                (SELECT category_id FROM clues
+                WHERE value = " + value_filter.to_s + ")
+             AND id IN 
+                (SELECT category_id FROM clues
+                 GROUP BY category_id
+                 HAVING COUNT(category_id) = 5)
+             ORDER BY Random()
+             LIMIT " + @@num_categories.to_s)
+        self.save()
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
 
   root controller: :rooms, action: :index
+  
+ resources :admin do
+        collection do
+      get :populate
+    end
+  end
 
   resources :room_messages
+  
   resources :rooms do
         collection do
       get :buzzer
