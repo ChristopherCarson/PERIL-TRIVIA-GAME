d62a50dd845e192459238a03228bb7b3ec3e1940
Michael Rose <mirose@csumb.edu>
2019-04-24 05:23:38 +0000

    Cleaned up login test file

--- test/integration/login_test_test.rb
+++ test/integration/login_test_test.rb
@@ -1,14 +1,10 @@
 require 'test_helper'
 
 class LoginTestTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
   test "access website" do
     https!
     get "/users/sign_in"
     assert_response :success
-    
     
   end
 end
